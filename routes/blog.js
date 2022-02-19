cbb81962703abe956002bddf7dd0b1884d803d69
Jeff Ripke <jripke74@gmail.com>
2022-02-18 17:00:09 -0800

    add async await to /posts route

--- routes/blog.js
+++ routes/blog.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res) {
 router.get('/posts', async function (req, res) {
   const query = `SELECT blog.posts.*, blog.authors.name AS author_name FROM blog.posts
                  INNER JOIN blog.authors ON posts.author_id = authors.id`;
-  const [ posts ] = await db.query(query);
+  const [posts] = await db.query(query);
   res.render('posts-list', { posts: posts });
 });
 
@@ -20,7 +20,7 @@ router.get('/new-post', async function (req, res) {
   res.render('create-post', { authors: authors });
 });
 
-router.post('/posts', function (req, res) {
+router.post('/posts', async function (req, res) {
   const data = [
     req.body.title,
     req.body.summary,
@@ -28,25 +28,36 @@ router.post('/posts', function (req, res) {
     req.body.author,
   ];
 
-  db.query(
+  await db.query(
     'INSERT INTO blog.posts (title, summary, body, author_id) VALUES (?)',
     [data]
   );
   res.redirect('/posts');
 });
 
-router.get('/posts/:id', async function(req, res) {
+router.get('/posts/:id', async function (req, res) {
   const query = `
     SELECT posts.*, authors.name AS author_name, authors.email FROM posts 
     INNER JOIN authors ON posts.author_id = authors.id
     WHERE posts.id = ?
   `;
 
-  const [ posts ] = await db.query(query, [req.params.id]);
+  const [posts] = await db.query(query, [req.params.id]);
 
   if (!posts || posts.length === 0) {
     return res.status(404).render('404');
   }
+
+  // const postData = {
+  //   ...posts[0],
+  //   date: posts[0].date.toISOString(),
+  //   humanReadableDate: posts[0].date.toLocaleDateString('en-US', {
+  //     weekday: 'long',
+  //     year: 'numeric',
+  //     month: 'long',
+  //     day: 'mumeric',
+  //   }),
+  // };
 
   res.render('post-detail', { post: posts[0] });
 });
